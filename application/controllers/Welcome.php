7f7b9cf6f3ae21336b869a14444758027d324e9d
trisamsul <trisamsul@gmail.com>
2016-11-18 13:39:01 +0700

    fresh bootstrap

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('starter.html');
+		$this->load->view('layout/header');
+		$this->load->view('index');
+		$this->load->view('layout/footer');
 	}
 }
---
+++ application/views/layout/header.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+	
+    <title>Dinamik 12</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="<?php echo base_url("assets/css/ie10-viewport-bug-workaround.css"); ?>" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url("assets/css/main.css"); ?>" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="<?php echo base_url("assets/js/ie-emulation-modes-warning.js"); ?>"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top nav">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Bootstrap starter template</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+		<br><br><br>
+      </div>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<?php echo base_url("assets/js/vendor/jquery.min.js")?>"><\/script>')</script>
+    <script src="<?php echo base_url("assets/js/bootstrap.min.js");?>"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="<?php echo base_url("assets/js/ie10-viewport-bug-workaround.js");?>"></script>
+  </body>
+</html>
