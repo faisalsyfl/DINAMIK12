08c1f016f1c923cc80915bea47321bfdc5c1083c
trisamsul <trisamsul@gmail.com>
2016-11-19 18:49:45 +0700

    section tentang

--- application/views/index.php
+++ application/views/index.php
@@ -54,3 +54,23 @@
 		    </div>
 		</div>
     </div><!-- /.container -->
+	<div id="tentang" class="container">
+		<div class="row">
+			<div class="col-md-4 col-md-offset-1 home-text">
+				<h3 class="title">
+					<img class="ss-ornament" src="<?php echo base_url("assets/img/ss-left-or.png"); ?>">
+					<b>TENTANG DINAMIK</b>
+					<img class="ss-ornament" src="<?php echo base_url("assets/img/ss-right-or.png"); ?>">
+				</h3>
+				<br>
+				<p class="tentang-text">
+					<b>DINAMIK</b><br>
+					Dies Natalis Mahasiswa Ilmu Komputer<br>
+					merupakan perayaan ulang tahun Keluarga Mahasiswa Ilmu Komputer (KEMAKOM)<br>
+					Pada tahun ini DINAMIK akan menginjak angka ke-12
+				</p>
+				<br>
+				<button type="button" class="btn-tentang">SELENGKAPNYA</button>
+			</div>
+		</div>
+    </div><!-- /.container -->
