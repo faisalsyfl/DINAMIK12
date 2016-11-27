41398f90d4c04de8c4a1ad2e61972aa8754465a8
mfr-fauzirahman <mfr.fauzirahman@gmail.com>
2016-11-28 00:06:55 +0700

    rekonfigurasi ulang lokasi plugin adminLTE, jadi ke $root/plugins penambahan controller & view basic layout dashboard admin akses via $root/dashboard/admin

--- assets/js/adminlte/pages/dashboard2.js
+++ assets/js/adminlte/pages/dashboard2.js
@@ -10,80 +10,6 @@ $(function () {
   //-----------------------
   //- MONTHLY SALES CHART -
   //-----------------------
-
-  // Get context with jQuery - using jQuery's .get() method.
-  var salesChartCanvas = $("#salesChart").get(0).getContext("2d");
-  // This will get the first returned node in the jQuery collection.
-  var salesChart = new Chart(salesChartCanvas);
-
-  var salesChartData = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-      {
-        label: "Electronics",
-        fillColor: "rgb(210, 214, 222)",
-        strokeColor: "rgb(210, 214, 222)",
-        pointColor: "rgb(210, 214, 222)",
-        pointStrokeColor: "#c1c7d1",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgb(220,220,220)",
-        data: [65, 59, 80, 81, 56, 55, 40]
-      },
-      {
-        label: "Digital Goods",
-        fillColor: "rgba(60,141,188,0.9)",
-        strokeColor: "rgba(60,141,188,0.8)",
-        pointColor: "#3b8bba",
-        pointStrokeColor: "rgba(60,141,188,1)",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(60,141,188,1)",
-        data: [28, 48, 40, 19, 86, 27, 90]
-      }
-    ]
-  };
-
-  var salesChartOptions = {
-    //Boolean - If we should show the scale at all
-    showScale: true,
-    //Boolean - Whether grid lines are shown across the chart
-    scaleShowGridLines: false,
-    //String - Colour of the grid lines
-    scaleGridLineColor: "rgba(0,0,0,.05)",
-    //Number - Width of the grid lines
-    scaleGridLineWidth: 1,
-    //Boolean - Whether to show horizontal lines (except X axis)
-    scaleShowHorizontalLines: true,
-    //Boolean - Whether to show vertical lines (except Y axis)
-    scaleShowVerticalLines: true,
-    //Boolean - Whether the line is curved between points
-    bezierCurve: true,
-    //Number - Tension of the bezier curve between points
-    bezierCurveTension: 0.3,
-    //Boolean - Whether to show a dot for each point
-    pointDot: false,
-    //Number - Radius of each point dot in pixels
-    pointDotRadius: 4,
-    //Number - Pixel width of point dot stroke
-    pointDotStrokeWidth: 1,
-    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-    pointHitDetectionRadius: 20,
-    //Boolean - Whether to show a stroke for datasets
-    datasetStroke: true,
-    //Number - Pixel width of dataset stroke
-    datasetStrokeWidth: 2,
-    //Boolean - Whether to fill the dataset with a color
-    datasetFill: true,
-    //String - A legend template
-    legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%=datasets[i].label%></li><%}%></ul>",
-    //Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-    maintainAspectRatio: true,
-    //Boolean - whether to make the chart responsive to window resizing
-    responsive: true
-  };
-
-  //Create the line chart
-  salesChart.Line(salesChartData, salesChartOptions);
-
   //---------------------------
   //- END MONTHLY SALES CHART -
   //---------------------------
@@ -99,37 +25,49 @@ $(function () {
       value: 700,
       color: "#f56954",
       highlight: "#f56954",
-      label: "Chrome"
+      label: "CSPC"
+    },
+	{
+      value: 221,
+      color: "#f56954",
+      highlight: "#f56954",
+      label: "Keamanan Jaringan"
+    },
+	{
+      value: 333,
+      color: "#f56954",
+      highlight: "#f56954",
+      label: "LCA"
     },
     {
       value: 500,
       color: "#00a65a",
       highlight: "#00a65a",
-      label: "IE"
+      label: "LCW"
     },
     {
       value: 400,
       color: "#f39c12",
       highlight: "#f39c12",
-      label: "FireFox"
+      label: "LDG"
     },
     {
       value: 600,
       color: "#00c0ef",
       highlight: "#00c0ef",
-      label: "Safari"
+      label: "OTIK"
     },
     {
       value: 300,
       color: "#3c8dbc",
       highlight: "#3c8dbc",
-      label: "Opera"
+      label: "PCA"
     },
     {
       value: 100,
       color: "#d2d6de",
       highlight: "#d2d6de",
-      label: "Navigator"
+      label: "Robot Line Follower"
     }
   ];
   var pieOptions = {
@@ -156,7 +94,7 @@ $(function () {
     //String - A legend template
     legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
     //String - A tooltip template
-    tooltipTemplate: "<%=value %> <%=label%> users"
+    tooltipTemplate: "<%=value %> <%=label%> peserta"
   };
   //Create pie or douhnut chart
   // You can switch between pie and douhnut using the method below.
---
+++ assets/js/custom/piechart2.js
@@ -0,0 +1,88 @@
+//-------------
+//- PIE CHART -
+//-------------
+// Get context with jQuery - using jQuery's .get() method.
+var pieChartCanvas = $("#pieChart2").get(0).getContext("2d");
+var pieChart = new Chart(pieChartCanvas);
+var PieData = [
+{
+  value: 700,
+  color: "#f56954",
+  highlight: "#f56954",
+  label: "Kategori 1"
+},
+{
+  value: 221,
+  color: "#f56954",
+  highlight: "#f32111",
+  label: "Kategori 2"
+},
+{
+  value: 333,
+  color: "#325fee",
+  highlight: "#f56954",
+  label: "Kategori 3"
+},
+{
+  value: 500,
+  color: "#00a65a",
+  highlight: "#00a65a",
+  label: "Kategori 4"
+},
+{
+  value: 400,
+  color: "#f39c12",
+  highlight: "#f39c12",
+  label: "Kategori 5"
+},
+{
+  value: 600,
+  color: "#00c0ef",
+  highlight: "#00c0ef",
+  label: "Kategori 6"
+},
+{
+  value: 300,
+  color: "#3c8dbc",
+  highlight: "#3c8dbc",
+  label: "Kategori 7"
+},
+{
+  value: 100,
+  color: "#d2d6de",
+  highlight: "#d2d6de",
+  label: "Kategori 8"
+}
+];
+var pieOptions = {
+//Boolean - Whether we should show a stroke on each segment
+segmentShowStroke: true,
+//String - The colour of each segment stroke
+segmentStrokeColor: "#fff",
+//Number - The width of each segment stroke
+segmentStrokeWidth: 1,
+//Number - The percentage of the chart that we cut out of the middle
+percentageInnerCutout: 50, // This is 0 for Pie charts
+//Number - Amount of animation steps
+animationSteps: 100,
+//String - Animation easing effect
+animationEasing: "easeOutBounce",
+//Boolean - Whether we animate the rotation of the Doughnut
+animateRotate: true,
+//Boolean - Whether we animate scaling the Doughnut from the centre
+animateScale: false,
+//Boolean - whether to make the chart responsive to window resizing
+responsive: true,
+// Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+maintainAspectRatio: false,
+//String - A legend template
+legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
+//String - A tooltip template
+tooltipTemplate: "<%=label%> <%=value %> peserta"
+};
+//Create pie or douhnut chart
+// You can switch between pie and douhnut using the method below.
+pieChart.Doughnut(PieData, pieOptions);
+//-----------------
+//- END PIE CHART -
+//-----------------
