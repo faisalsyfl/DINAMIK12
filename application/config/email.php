e6d97afedd998321519ba0c9879c1e3031f66e09
mfr-fauzirahman <mfr.fauzirahman@gmail.com>
2017-01-08 09:28:58 +0700

    Email config

--- application/config/email.php
+++ application/config/email.php
@@ -38,7 +38,7 @@ $config = Array(
 	'protocol'  => 'smtp',
 	'smtp_host' => 'ssl://smtp.googlemail.com',
 	'smtp_port' => 465,
-	'smtp_user' => '',
-	'smtp_pass' => '',
+	'smtp_user' => 'dinamik.cs@upi.edu',
+	'smtp_pass' => '!dinamik12csupi#',
 	'mailtype'  => 'html', 
 );
